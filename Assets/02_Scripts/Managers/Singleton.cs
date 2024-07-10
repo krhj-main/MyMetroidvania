68713059f1c7f62ed99c12a1cdf28f9670babf2f
krhj-main <dygks6666@gmail.com>
2024-07-11 03:10:34 +0900

    07.11.Tur ver 0.0.2
    퇴근 후 새벽작업
    캐릭터 이동, 그라운드 체크, 플립, 애니메이션 등 기본기능 작업 중

--- Assets/02_Scripts/Managers/Singleton.cs
+++ Assets/02_Scripts/Managers/Singleton.cs
@@ -1,9 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Singleton : MonoBehaviour
 {
+    static Singleton instance= null;
+
+    public static Singleton Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Assets/02_Scripts/Players/PlayerController.cs
+++ Assets/02_Scripts/Players/PlayerController.cs
@@ -7,16 +7,52 @@ public class PlayerController : MonoBehaviour
     Vector2 moveAxis;
     Rigidbody2D rig;
     Animator anim;
+    Singleton gm;
+    PlayerStateList pState;
+
+
+
+
+
+    [Header ("플레이어 이동속도")]
     [SerializeField] float moveSpeed;
 
+    [Space (5)]
+    [Header("플레이어 점프")]
     [SerializeField] float jumpForce;
-    [SerializeField] GameObject groundcheker;
+    [SerializeField] int jumpExtra;
+    int jumpCurrent;
+
+    [Space(5)]
+    [Header("그라운드 체크")]
+    [SerializeField] bool Debugging;
+    [SerializeField] Transform groundChecker;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float groundOffset;
+
+    [Space(5)]
+    [Header("플레이어 공격")]
+    [SerializeField] float playerAttackSpeed;
+    [SerializeField] float playerAttackCooltime;
+
+    [Space(5)]
+    [Header("플레이어 반동")]
+    [SerializeField] float recoilForce;
+    [SerializeField] float recoilTime;
+    
+
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        gm = GetComponent<Singleton>();
         rig = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        pState = GetComponent<PlayerStateList>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,28 +60,55 @@ public class PlayerController : MonoBehaviour
     {
         GetInput();
         ActiveMove();
+        ActiveFlip();
     }
     void GetInput()
     {
         moveAxis = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
-        if (Grounded())
+        anim.SetBool("isJump", !Grounded());
+        if (Input.GetKeyDown(KeyCode.C) && (jumpCurrent > 0 || Grounded()))
         {
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                ActiveJump();
-            }
+            
+            jumpCurrent--;
+            ActiveJump();
         }
     }
     void ActiveMove()
     {
         rig.velocity = new Vector2(moveAxis.x * moveSpeed, rig.velocity.y);
+        anim.SetBool("isWalk",moveAxis.x!=0);
+    }
+    void ActiveFlip()
+    {
+        if (moveAxis.x > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+            pState.lookRight = true;
+        }
+        if (moveAxis.x < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+            pState.lookRight = false;
+        }
     }
     void ActiveJump()
     {
         rig.velocity = new Vector2(rig.velocity.x, jumpForce);
+        if (Grounded())
+        {
+            jumpCurrent = jumpExtra;
+        }
     }
     bool Grounded()
     {
-        return true;
+        bool isGround = Physics2D.OverlapCircle(groundChecker.position, groundOffset, groundLayer);
+        return isGround;
+    }
+    private void OnDrawGizmos()
+    {
+        if (!Debugging) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(groundChecker.position,groundOffset);
     }
 }
