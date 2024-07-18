9e9582116b946174fbd687b16121d1a74be0b1a2
krhj-main <dygks6666@gmail.com>
2024-07-19 02:23:33 +0900

    07.18.Tur ver.0.2.1
    퇴근 후 새벽작업 엘리베이터 완성, 상호작용키가 좀 씹히는 버그가 있음.

--- Assets/02_Scripts/Interact/Elevator.cs
+++ Assets/02_Scripts/Interact/Elevator.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform upFloor, downFloor;
+    [SerializeField] float elevatorSpeed;
+    PlayerController player;
+
+    bool activating=false;
+    float dist;
+    private void Awake()
     {
-        
+        player = PlayerController.Instance;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (activating)
+        {
+            if(dist > 0.5f)
+            StartCoroutine(ElevatorMove(upFloor));
+
+            else
+            StartCoroutine(ElevatorMove(downFloor));
+        }
+    }
+
+
+    void UpdateElevator()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            activating = true;
+            dist = Vector2.Distance(transform.position, upFloor.position);
+            player.gameObject.transform.SetParent(transform);
+        }
+    }
+    IEnumerator ElevatorMove(Transform _movePos)
+    {
+        Vector2 _verticalDir = (transform.position - _movePos.position).normalized;
+        transform.position = Vector2.MoveTowards(transform.position, _movePos.position, elevatorSpeed * Time.deltaTime);
+        if (Mathf.Abs(_verticalDir.y) < 0.1f)
+        {
+            activating = false;
+            player.gameObject.transform.SetParent(null);
+            DontDestroyOnLoad(player.gameObject);
+        }
+        yield return null;
+    }
+
+    private void OnTriggerStay2D(Collider2D _col)
+    {
+        if (_col.tag.Contains("Player"))
+        {
+            UpdateElevator();
+        }
     }
 }
